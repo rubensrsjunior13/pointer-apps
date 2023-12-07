ea161ce630bc6a0342e3b0d3b5d5395192b8ee82
EC2 Default User <ec2-user@ip-172-31-41-114.ec2.internal>
2023-12-07 23:07:51 +0000

    Add terraform files

---
+++ variables.tf
@@ -0,0 +1,10 @@
+## Conteúdo do variables.tf
+variable vpc_id {
+  type        = string
+  description = "Informe sua VPC ID"
+}
+
+variable subnet_id {
+  type        = string
+  description = "Informe sua subnet id"
+}
