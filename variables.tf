def7fe26961aca51e0379c5130a2225c89ffb500
rubensrsjunior13 <52670930+rubensrsjunior13@users.noreply.github.com>
2023-12-31 17:14:56 -0300

    ok

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 ## Conteúdo do variables.tf
 variable vpc_id {
   type        = string
-  description = "vpc-04bc7f3f0ffb16056"
+  description = "Informe sua VPC ID"
 }
 
 variable subnet_id {
   type        = string
-  description = "subnet-06a4424908e7607d2"
+  description = "Informe sua subnet id"
 }
