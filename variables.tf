e3b281a033159350141108faa600817e31bdb5b5
rubensrsjunior13 <52670930+rubensrsjunior13@users.noreply.github.com>
2023-12-31 17:05:04 -0300

    ok

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 ## Conteúdo do variables.tf
 variable vpc_id {
   type        = string
-  description = "Informe sua VPC ID"
+  description = "vpc-04bc7f3f0ffb16056"
 }
 
 variable subnet_id {
   type        = string
-  description = "Informe sua subnet id"
+  description = "subnet-06a4424908e7607d2"
 }
